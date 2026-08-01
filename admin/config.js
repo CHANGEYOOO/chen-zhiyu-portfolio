6128676ddb009abb5c3f61b6a2921e8b957f1b29
CHANGEYOOO <98197480+CHANGEYOOO@users.noreply.github.com>
2026-08-01 23:38:03 +0800

    Ship public Supabase admin configuration

---
+++ admin/config.js
@@ -0,0 +1,4 @@
+window.PORTFOLIO_ADMIN_CONFIG = {
+  supabaseUrl: "https://ykhvsawnjjunhiffzbjz.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlraHZzYXduamp1bmhpZmZ6Ymp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1ODYxNTAsImV4cCI6MjEwMTE2MjE1MH0.Rl_Za0lRL-PJksANN8LSLdx5mo7zCRjq_069eq4i3dk",
+};
